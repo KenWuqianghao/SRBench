5b2bd5362e0a838aa121430f450a5e72e56f3a68
Ken Wu (k6wu) <k6wu@gpu-sas01.math.private.uwaterloo.ca>
2024-10-08 10:32:30 -0400

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,38 @@
+# note: make sure conda environment is installed 
+# before running install. see configure.sh
+conda activate srbench
+conda info
+
+failed=()
+
+# install methods
+echo "///////////////////////////"
+echo "installing GP methods..."
+echo "///////////////////////////"
+
+# move to methods folder
+cd experiment/methods/src/
+
+# install all methods
+for install_file in $(ls *.sh) ; do
+    echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+    echo "Running $install_file"
+    echo "////////////////////////////////////////////////////////////////////////////////"
+
+    bash $install_file
+
+    if [ $? -gt 0 ]
+    then
+        failed+=($install_file)
+    fi
+
+    echo "////////////////////////////////////////////////////////////////////////////////"
+    echo "Finished $install_file"
+    echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+done
+
+if [ ${#failed[@]} -gt 0 ] ; then
+    echo "${#failed[@]} installs failed: ${failed}"
+else
+    echo "All installations completed successfully."
+fi
